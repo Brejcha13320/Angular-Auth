171e2522914bd1481cfe49ed46c5d57007551739
Brejcha13320 <davidmejia-2000@outlook.com>
2023-10-19 01:09:35 -0500

    ajustes en el title, componentes de tablas

--- src/app/home/components/index.ts
+++ src/app/home/components/index.ts
@@ -1,8 +1,11 @@
 import { AboutComponent } from './about/about.component';
 import { CategoryComponent } from './category/category.component';
+import { TableCategoryComponent } from './category/table-category/table-category.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { TableListComponent } from './list/table-list/table-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TableUserComponent } from './users/table-user/table-user.component';
 import { UsersComponent } from './users/users.component';
 
 export const COMPONENTS = [
@@ -12,4 +15,7 @@ export const COMPONENTS = [
   ListComponent,
   UsersComponent,
   AboutComponent,
+  TableListComponent,
+  TableCategoryComponent,
+  TableUserComponent,
 ];
